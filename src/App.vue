ffe9366fd89091ea5f6f96ddb32216b7f3d3ed0c
JaxNext <qianjunying@keep.com>
2024-11-07 19:22:58 +0800

    feature: check if usable

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,20 @@
 import NotSupport from '@/components/NotSupport.vue'
 import DarkMode from '@/components/DarkMode.vue'
 import Translator from '@/components/Translator.vue'
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import Toaster from '@/components/ui/toast/Toaster.vue'
+import { checkDetectorUsability, checkTranslatorUsability } from '@rejax/browser-ai-translator'
 
-const isSupport = ref(true)
+const isSupport = ref(false)
+
+onMounted(async () => {
+  const canDetect = await checkDetectorUsability()
+  const canTranslate = await checkTranslatorUsability({
+    sourceLanguage: 'en',
+    targetLanguage: 'zh',
+  })
+  isSupport.value = canDetect.available && canTranslate.available
+})
 </script>
 
 <template>
--- src/components/NotSupport.vue
+++ src/components/NotSupport.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-center dark:text-slate-50">
     <h2 class="text-2xl font-bold mt-24">你的浏览器还没有解锁该功能</h2>
-    <p class="text-xl mt-4">解锁攻略请戳这里：<a href="https://github.com/antfu/vitesse-nuxt3/blob/main/src/components/NotSupport.vue" target="_blank" class="text-blue-500">todo</a></p>
+    <p class="text-xl mt-4">请戳这里：<a href="https://juejin.cn/post/7427326888055439410" target="_blank" class="text-blue-500">解锁攻略</a></p>
   </div>
 </template>
 <script setup lang="ts">
