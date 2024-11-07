b6c092fb229163767acc9f8764776f1900fd3df3
JaxNext <qianjunying@keep.com>
2024-11-07 18:45:39 +0800

    change dist dir name

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'chrome-ai-translator',
+    outDir: 'translator',
     rollupOptions: {
       output: {
         sanitizeFileName(name) {
